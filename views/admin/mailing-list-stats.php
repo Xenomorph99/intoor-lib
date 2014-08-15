714a2531208b5fd36a1f7bd36813bb69e383c487
Colton <Alekhen@gmail.com>
2014-08-14 20:21:50 -0700

    Quick Update to Mailing Lists Stats View Formatting

--- views/admin/mailing-list-stats.php
+++ views/admin/mailing-list-stats.php
@@ -17,14 +17,12 @@
 
 	<table class="form-table">
 		<tbody>
-
 			<?php foreach( $defaults as $name => $value ) : ?>
 			<tr>
 				<th scope="row"><?php echo $title = ( !empty( $value[2] ) ) ? $value[2] : ucwords( $name ); ?></th>
 				<td style="font-size:1.2em;"><?php echo $stat = get_option( $id . '_' . $name, $value[0] ) ?></td>
 			</tr>
 			<?php endforeach; ?>
-
 		</tbody>
 	</table>
 
