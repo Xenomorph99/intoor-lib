7e176099ddc41d9ff93df479bc4ae50cb8449dd9
Colton <Alekhen@gmail.com>
2014-10-16 11:52:48 -0700

    Refactored 'Shortcodes' Class

--- load.php
+++ load.php
@@ -27,7 +27,7 @@ require_once INTOOR_META_BOX_CLASS;
 require_once INTOOR_POPULAR_CLASS;
 require_once INTOOR_POST_TYPE_CLASS;
 //require_once INTOOR_QUICK_EDIT_CLASS;
-require_once INTOOR_SHORTCODE_CLASS;
+require_once INTOOR_SHORTCODES_CLASS;
 require_once INTOOR_SOCIAL_CLASS;
 //require_once INTOOR_TAXONOMY_CLASS;
 require_once INTOOR_TRACKING_CLASS;
